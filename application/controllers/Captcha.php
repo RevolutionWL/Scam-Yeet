b7e362965bfe776f33bd7c05f79d4043f6ff85a1
William Yong <w.yong@uqconnect.edu.au>
2020-05-19 20:39:39 +1000

    Added Captcha function (seperate url)

---
+++ application/controllers/Captcha.php
@@ -0,0 +1,84 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Captcha extends CI_Controller
+{
+    function __construct() {
+        parent::__construct();
+        
+        // Load session library
+        $this->load->library('session');
+        
+        // Load the captcha helper
+        $this->load->helper('captcha');
+    }
+    
+    public function index(){
+        
+        // If captcha form is submitted
+        if($this->input->post('submit')){
+            $inputCaptcha = $this->input->post('captcha');
+            $sessCaptcha = $this->session->userdata('captchaCode');
+            if($inputCaptcha === $sessCaptcha){
+                echo 'Captcha code matched.';
+            }else{
+                echo 'Captcha code does not match, please try again.';
+            }
+        }
+        
+        // Captcha configuration
+        $config = array(
+            'img_path'      => 'Icaptcha/',
+            'img_url'       => base_url().'Icaptcha/',
+            'font_path'     => './path/to/fonts/texb.ttf',
+            'img_width'     => '160',
+            'img_height'    => 50,
+            'word_length'   => 8,
+            'font_size'     => 30,
+            'pool'          =>  'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+            'colors'        => array(
+                'background' => array(0, 0, 0),
+                'border' => array(255, 255, 255),
+                'text' => array(255, 255, 255),
+                'grid' => array(255, 40, 40)
+        )
+        );
+        $captcha = create_captcha($config);
+        
+        // Unset previous captcha and set new captcha word
+        $this->session->unset_userdata('captchaCode');
+        $this->session->set_userdata('captchaCode', $captcha['word']);
+        
+        // Pass captcha image to view
+        $data['captchaImg'] = $captcha['image'];
+        
+        // Load the view
+        $this->load->view('captcha', $data);
+    }
+    
+    public function refresh(){
+        // Captcha configuration
+        $config = array(
+            'img_path'      => 'Icaptcha/',
+            'img_url'       => base_url().'Icaptcha/',
+            'font_path'     => './path/to/fonts/texb.ttf',
+            'img_width'     => '160',
+            'img_height'    => 50,
+            'word_length'   => 8,
+            'font_size'     => 30,
+            'pool'          =>  'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+            'colors'        => array(
+                'background' => array(0, 0, 0),
+                'border' => array(255, 255, 255),
+                'text' => array(255, 255, 255),
+                'grid' => array(255, 40, 40)
+        )
+        );
+        $captcha = create_captcha($config);
+        
+        // Unset previous captcha and set new captcha word
+        $this->session->unset_userdata('captchaCode');
+        $this->session->set_userdata('captchaCode',$captcha['word']);
+        
+        // Display captcha image
+        echo $captcha['image'];
+    }
+}
---
+++ application/views/captcha.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- captcha refresh code -->
+        <script>
+        $(document).ready(function(){
+            $('.refreshCaptcha').on('click', function(){
+                $.get('<?php echo base_url().'captcha/refresh'; ?>', function(data){
+                    $('#captImg').html(data);
+                });
+            });
+        });
+        </script>
+
+        <title>User Verification</title>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+        <h4>Submit Captcha Code</h4>
+        <p id="captImg"><?php echo $captchaImg; ?></p>
+        <p>Can't read the image? click <a href="javascript:void(0);" class="refreshCaptcha">here</a> to refresh.</p>
+        <form method="post">
+            Enter the code : 
+            <input type="text" name="captcha" value=""/>
+            <input type="submit" name="submit" value="SUBMIT"/>
+        </form>
+
+        </div>
+
+    </body>
+
+</html>
