3f35ea6e190a8787f5968a80f23c6c6d78033d48
William Yong <w.yong@uqconnect.edu.au>
2020-05-19 21:48:10 +1000

    Updated captcha action on checking
    Wrong - Show another captcha and error message
    Correct - Redirect to login

--- application/views/captcha.php
+++ application/views/captcha.php
@@ -30,7 +30,7 @@
         <h4>Submit Captcha Code</h4>
         <p id="captImg"><?php echo $captchaImg; ?></p>
         <p>Can't read the image? click <a href="javascript:void(0);" class="refreshCaptcha">here</a> to refresh.</p>
-        <form method="post">
+        <form method="post" action="<?php echo base_url();?>captcha/check">
             Enter the code : 
             <input type="text" name="captcha" value=""/>
             <input type="submit" name="submit" value="SUBMIT"/>
