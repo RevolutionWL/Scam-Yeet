e2161087896d882f45269635d20b81d33fbf19df
William Yong <w.yong@uqconnect.edu.au>
2020-05-27 17:23:17 +1000

    The no video file for when video doesnt exist

---
+++ application/views/novideo.php
@@ -0,0 +1,59 @@
+<html>  
+<head>
+
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+        <style type = "text/css">
+                    
+            body {
+                color: #fff;
+                background: #63738a;
+                font-family: 'Roboto', sans-serif;
+            }
+            video {
+                background: black;
+                }
+            .container {
+
+                margin-left: 50px;
+
+
+            }
+            /* width */
+::-webkit-scrollbar {
+  width: 7px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
+        </style>
+
+<title>UwU Video</title>
+</head>
+<body>
+
+<div class="container">
+
+    <h2>Video is non-Existences Boo~</h2>
+
+</div>
+
+
+</body>
+</html>
